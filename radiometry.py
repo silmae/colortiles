a9278f3171411a84808fce16f0e5f5174211a99b
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-10-09 17:28:14 -0400

    remove to_refl (not performant for many files)

--- radiometry.py
+++ radiometry.py
@@ -2,17 +2,11 @@ import xarray as xr
 import numpy as np
 
 
-def to_refl(data, dark, white):
-	white = sub_dark(white, dark)
-	data = sub_dark(data, dark)
-	return data / white
-
-
 def sub_dark(arr, dark, method='zeroclip'):
     """Subtract dark from arr"""
     methods = {
 		'zeroclip': _subclip,
-		'asfloat': _floatsub
+		'asfloat': _floatsub,
 		}
     return methods[method](arr, dark)
 
