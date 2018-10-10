b73b2b3bb4b4c2eee2ff0fc3b344db328fe62e39
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-10-10 12:29:06 -0400

    Add parsed coordinates

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ Utility functions.
 import xarray as xr
 from glob import glob
 import os.path as osp
+from datetime import datetime
 
 
 def band2wl(x):
@@ -12,11 +13,20 @@ def band2wl(x):
 
 
 def read_ENVI_ds(files):
-    return xr.Dataset(
-        data_vars=dict((simple_name(f), xr.open_rasterio(f)) for f in files)
-    )
+    data = dict((simple_name(f), xr.open_rasterio(f)) for f in files)
+    
+    for s, da in data.items():
+        sparts = s.split('_')
+        da.coords['time'] = parse_time('_'.join(sparts[3:5]))
+        da.coords['material'] = ' '.join(sparts[5:])
+    
+    return xr.concat(data.values(), dim='time')
 
 
 def simple_name(path):
-    """Extract more relevant part of the path"""
-    return osp.basename(path)[22:]
+    """Extract more relevant part of the filename"""
+    return osp.splitext(osp.basename(path))[0]
+
+
+def parse_time(s):
+    return datetime.strptime(s, '%d-%m-%Y_%H.%M.%S')
