9d30642ead21c2dc1784bf9ff034a5f02c567401
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-10-11 10:58:04 -0400

    Just use all white tiles, fix calculation

--- calc_refl.py
+++ calc_refl.py
@@ -1,35 +1,28 @@
 import xarray as xr
 import sys
 
-# White references to calculate reflectance with
-reftimes = [
-    '2018-10-03T10:59:48.000000000',
-    '2018-10-03T13:17:29.000000000',
-    '2018-10-03T13:33:40.000000000',
-    ]
-
 def main(argv):
     input = argv[0]
     output = argv[1]
-    print(f'Calculating reflectances from dataset {input} using refs \n {reftimes}')
+    print(f'Calculating reflectances from dataset {input}')
     print(f'and saving result to {output}.')
     print(80*'=')
     print('Reading dataset')
     ds = xr.open_dataset(input)
     ds.load()
 
-    refs = ds.sel(time=reftimes)
+    refs = ds.where(ds.material == 'White', drop=True)
+    print(f'Calculating reflectances using references {refs.time.values}')
     refls = []
-    print('Calculating reflectances...')
     for t, ref in refs.groupby('time'):
         print(f'Reference {t}')
-        refl = ds['dark_corrected_dn'] / ref
+        refl = ds['dark_corrected_dn'] / ref['dark_corrected_dn']
         refl.coords['reference'] = t
         refls.append(refl)
     
     print(f'Saving results to {output}')
     ds['reflectance'] = xr.concat(refls, dim='reference')
-    ds.drop('dark_corrected_dn')
+    ds = ds.drop('dark_corrected_dn')
     ds.to_netcdf(output)
 
 
