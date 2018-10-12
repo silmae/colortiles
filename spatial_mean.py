a39c6d25057caa96acd2ce08bda1aa0d2084a10d
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-10-12 10:37:22 -0400

    Apply desired crop and store the coordinates

--- spatial_mean.py
+++ spatial_mean.py
@@ -2,14 +2,17 @@ import xarray as xr
 import utils as u
 import sys
 
+
 def main(argv):
     inputfile = argv[0]
     variable = argv[1]
-    output = argv[2]
+    n = argv[2]
+    output = argv[3]
 
     outvar = 'mean_' + variable
-    confirmation = (f'Calculate spatial mean of variable {variable} in {inputfile}\n'
-                    f'and save it as {outvar} in {output}? [y/n]')
+    confirmation = (f'Calculate spatial mean of n x n center pixels of '
+                    f'variable {variable} in {inputfile}\n and save it '
+                    f'as {outvar} in {output}? [y/n]')
 
     ok = input(confirmation)
     if ok != 'y':
@@ -18,9 +21,14 @@ def main(argv):
 
     print(f'Opening dataset')
     ds = xr.open_dataset(inputfile)
-    ds['mean' + variable] = ds[variable].mean(dim=['x', 'y'])
+    center = u.crop_center(ds[variable], n)
+    ds['cropx'] = center.x
+    ds['cropy'] = center.y
+    ds['mean_' + variable] = center.mean(dim=['x', 'y'])
+    ds['var_' + variable] = center.var(dim=['x', 'y'])
     ds = ds.drop(variable)
     ds.to_netcdf(output)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
