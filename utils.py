460bd4393554f067e2937646c97c9be2eabf11ca
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-10-12 09:48:37 -0400

    Add utility for cropping a center part

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import xarray as xr
 from glob import glob
 import os.path as osp
 from datetime import datetime
+from math import ceil
 
 
 def band2wl(x):
@@ -33,4 +34,18 @@ def simple_name(path):
 
 
 def parse_time(s):
+    """Parse the time format of the camera-given filenames to datetime."""
     return datetime.strptime(s, '%d-%m-%Y_%H.%M.%S')
+
+
+def crop_center(data, n):
+    """Select a n x n -sized square in the center of the image.
+    For odd width or height, bias towards index 0.
+    """
+    cx, cy = data.sizes['x'] // 2, data.sizes['y'] // 2
+    start, end = n // 2, ceil(n / 2)
+    crop = {
+        'x': slice(cx - start, cx + end),
+        'y': slice(cy - start, cy + end)
+         }
+    return data.isel(**crop)
