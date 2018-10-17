f13ae7dfa9b531437ad458cf2b6be1300656ec4e
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-10-17 16:50:19 -0400

    Add model fitting (untested)

--- model.py
+++ model.py
@@ -2,6 +2,70 @@
 Spectrophotometric model for calibration using color tiles
 """
 
+import numpy as np
+import xarray as xr
+from scipy.linalg import lstsq
+
+
+def fit_dataarray(R, R_ref):
+    """Fit the model parameters to the given measurements R and R_ref.
+    
+    Parameters
+    ----------
+    R : xr.DataArray
+        Measured reflectance spectra as a DataArray containing wavelength
+        coordinates.
+    R_ref : xr.DataArray
+        Reflectance spectra of the reference with matching wavelength
+        coordinates.
+    
+    """
+
+    ds = xr.Dataset(
+        data_vars={
+            'deltaR': R_ref - R,
+            'R': R,
+            'dR': spectral_derivative(R, 1),
+            'ddR': spectral_derivative(R, 2)
+        }
+    )
+
+    A = _deltaR_matrix(
+        ds['R'].values.ravel(),
+        ds['dR'].values.ravel(),
+        ds['ddR'].values.ravel(),
+    )
+
+    return lstsq(A, ds['deltaR'].values.ravel())
+
+
+def _deltaR_matrix(R, dR, ddR):
+    """Matrix corresponding to the function _deltaR
+         
+    Parametersq
+    ----------
+    R : np.array
+        N x 1 vector of reflectance values
+    dR : np.array
+        N x 1 vector of differentiated reflectance values
+    ddR : np.array
+        N x 1 vector of twice differentiated reflectance values
+
+    Result
+    ------
+    np.array
+        Matrix for calculating the model
+
+    """
+    return np.stack([
+        np.ones_like(R),
+        R,
+        dR,
+        ddR,
+        (100 - R) * R
+        ]
+    ).T
+
 
 def deltaR(R, c1, c2, c3, c4, c5):
     """Difference from the assigned reflectance
