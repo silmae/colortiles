d67c1de670fd22f638ad80cc81f7d6223d378d98
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-10-17 14:47:58 -0400

    Allow different variables using parameter

--- calc_refl.py
+++ calc_refl.py
@@ -5,6 +5,8 @@ import sys
 def main(argv):
     inputfile = argv[0]
     output = argv[1]
+    variable = argv[2]
+
     print(f'Calculating reflectances from dataset {inputfile}')
     print(f'and saving result to {output}.')
     print(80*'=')
@@ -17,13 +19,13 @@ def main(argv):
     refls = []
     for t, ref in refs.groupby('time'):
         print(f'Reference {t}')
-        refl = ds['dark_corrected_dn'] / ref['dark_corrected_dn']
+        refl = ds[variable] / ref[variable]
         refl.coords['reference'] = t
         refls.append(refl)
 
     print(f'Saving results to {output}')
     ds['reflectance'] = xr.concat(refls, dim='reference')
-    ds = ds.drop('dark_corrected_dn')
+    ds = ds.drop(variable)
     ds.to_netcdf(output)
 
 
