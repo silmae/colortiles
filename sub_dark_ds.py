8d1b445f456b43786dbf1b2b37a15b6adb9cb9a0
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-10-24 14:52:09 -0400

    Change sub_dark_ds to operate on datasets

--- sub_dark_ds.py
+++ sub_dark_ds.py
@@ -1,39 +1,28 @@
 import sys
 import xarray as xr
-from glob import glob
 from radiometry import sub_dark
-from utils import read_ENVI_data
-
-from warnings import filterwarnings
-filterwarnings('ignore', 'Dataset has no geotransform set')
 
 
 def main(argv):
-    pattern = argv[0]
+    inputfile = argv[0]
     darkfile = argv[1]
     outfile = argv[2]
 
-    inputs = sorted(glob(pattern))
-
-    print(f'Subtracting {darkfile} from the following files')
+    print(f'Subtracting  (filename coordinate) {darkfile}')
+    print(f'from variable dn in {inputfile}')
     print(f'and saving output dataset to {outfile}')
-    print(80 * '=')
-    for i in inputs:
-        print(i)
-    print(80 * '=')
     ok = input('Is this right? [y/n]')
     if ok != 'y':
         print('Aborting...')
         exit()
-    dark = xr.open_rasterio(darkfile)
-    print('Dark read, reading dataset')
-    ds = read_ENVI_data(inputs)
-    ds.load()
-    print('Data read, subtracting dark')
-    ds = ds.set_coords('wavelength')
-    ds['dark_corrected_dn'] = sub_dark(ds['dn'], dark)
+    
+    ds = xr.open_dataset(inputfile, chunks={'filename': 1})
+    dark = ds.sel({'filename': darkfile})
+    
+    print('Data and dark found, subtracting dark')
+    ds['dark_corrected_dn'] = sub_dark(ds['dn'], dark['dn'])
     ds = ds.drop('dn')
-    ds = ds.reset_coords()
+    ds.assign_attrs({'dark_reference': dark})
 
     print(f'Finished, saving to {outfile}')
     ds.to_netcdf(outfile)
