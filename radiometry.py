fb7126dd1f6a618ee26eddb8d0614ffec2a5e9a6
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-11-20 17:55:59 -0500

    Add reflectance calculation and linear fit to radiometry

--- radiometry.py
+++ radiometry.py
@@ -1,5 +1,43 @@
 import numpy as np
 import xarray as xr
+from scipy.stats import linregress
+
+
+def linear_regression(ds, x, y, dim):
+    """Compute a linear regression between two variables
+    grouped by given dimension.
+    
+    Parameters
+    ----------
+    ds : xr.Dataset
+      Dataset containing variables x and y with dimension dim.
+    x : str
+      Independent variable in the dataset.
+    y : str
+      Dependent variable in the dataset.
+    dim : str
+      Dimension to group the data by before the regression.
+    
+    Returns
+    -------
+    xr.Dataset
+      Dataset with the results of the regression along the given dimension.
+    """
+
+    def fit_band(ds):
+        x_ = ds[x].data.ravel()
+        y_ = ds[y].data.ravel()
+        res = linregress(x_, y_)
+        (ds['slope'],
+         ds['intercept'],
+         ds['rvalue'],
+         ds['pvalue'],
+         ds['stderr']) = res
+        return ds
+    
+    ds = ds.groupby(dim).apply(fit_band)
+
+    return ds
 
 
 def PTFE_reflectance_factor_45_0():
@@ -20,6 +58,31 @@ def PTFE_reflectance_factor_45_0():
       coords={'wavelength': wls}
     )
     return Rf
+
+
+def compute_reflectance(ds, data_variable, reference_variable):
+    """Compute reflectance given data and reference.
+  
+    Drops both used variables from the resulting dataset, keeping others.
+    
+    Parameters
+    ----------
+    ds : xr.Dataset
+      Dataset containing both the data and reference variables.
+    data_variable : str
+      Variable containing the data.
+    reference_variable : str
+      Variable containing the reference data.
+    
+    Returns
+    -------
+    xr.Dataset
+      Dataset with the computed reflectance.
+    """
+
+    ds['reflectance'] = ds[data_variable] / ds[reference_variable]
+    ds = ds.drop([data_variable, reference_variable])
+    return ds
 
 
 def sub_dark(arr, dark, method='default'):
